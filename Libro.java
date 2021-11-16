2bc22da6709b7052ce69cc04bea992fba871ff85
Daniel <supertuxcar@gmail.com>
2021-11-16 09:01:36 +0100

    paginas

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
+    private int paginas;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroPaginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        paginas = numeroPaginas;
     }
     
      /**
@@ -34,5 +36,12 @@ public class Libro {
      */
     public String imprimeTitulo() {
         return titulo;
+    }
+    
+     /**
+     * Devuelve el numero de paginas
+     */
+    public int numeroPaginas() {
+        return paginas;
     }
 }
